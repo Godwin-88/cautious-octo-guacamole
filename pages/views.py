9697de4d9d559b145ad3b8f87bd803eb3dcf9b3c
Godwin-88 <godwinedgar@gmail.com>
2024-08-04 00:13:26 +0300

    creating views in the portfolo

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from projects.models import Project
 
-def home(request):
-    return render(request,"pages/home.html",{})
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "projects/project_index.html", context)
+
+'''
+Next, you’ll need to create the project_detail() view function. 
+This function will have the primary key of the project that’s being 
+viewed as an additional argument:
+'''
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+    return render(request, "projects/project_detail.html", context)
